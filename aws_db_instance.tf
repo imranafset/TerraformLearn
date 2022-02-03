beea57655e44e5bec8dcf03e97779e795b39528e
imranafset <mi3081557@gmail.com>
2022-02-03 08:15:04 +0000

    Terraform AWS RDS Instance Demo

---
+++ aws_db_instance.tf
@@ -0,0 +1,17 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = file("${path.module}/confidential.txt")
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+
+
+#data "aws_db_instance" "database" {
+#  db_instance_identifier = "my-test-database"
+#}
